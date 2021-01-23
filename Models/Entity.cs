2479f8cd7d4963083499b5a736c06c6dc5b01f92
Matt Wrench <matt@mattwrench.com>
2021-01-23 16:14:51 -0600

    Add Entity class

---
+++ Models/Entity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interstellar.Models
+{
+    // Base class for all sprites
+    abstract class Entity
+    {
+        public Vector2 Position; // X/Y is at center of sprite
+        public Vector2 Velocity;
+        public float TopSpeed;
+        public Rectangle Bounds; // X/Y is at top-left of sprite
+        public float Rotation; // In degrees; 0 is pointing right
+
+        public Entity()
+        {
+            // Default values will be overwritten in child constructors
+            Position = new Vector2();
+            Velocity = new Vector2();
+            Bounds = new Rectangle();
+            Rotation = 0;
+        }
+
+        // Update Bounds.X/Y from Position.X/Y
+        public void SetBounds()
+        {
+            Bounds.X = (int)(Position.X - Bounds.Width / 2);
+            Bounds.Y = (int)(Position.Y - Bounds.Height / 2);
+        }
+    }
+}
