3d0b8ce266404f167c0af76447619a1f5611b1b8
Matt Wrench <matt@mattwrench.com>
2021-01-24 20:35:00 -0600

    Add ShipController class

---
+++ Controllers/ShipController.cs
@@ -0,0 +1,17 @@
+﻿using Interstellar.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interstellar.Controllers
+{
+    abstract class ShipController : EntityController
+    {
+        public ShipController(World w) : base(w)
+        {
+        }
+
+        protected abstract void setRotation(Ship ship); // Only ships will have changing rotation
+        protected abstract void handleAttack(Ship ship, float dt); // Only ships can attack
+    }
+}
