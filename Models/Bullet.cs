5ba33759499629ef28491135e68aa52b397d2716
Matt Wrench <matt@mattwrench.com>
2021-01-26 21:48:36 -0600

    Add Bullet class

---
+++ Models/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interstellar.Models
+{
+    class Bullet : Entity
+    {
+        public bool IsShotByPlayer;
+        public Bullet(Vector2 pos, Vector2 dir, bool shotByPlayer)
+        {
+            // Class attributes
+            Bounds.Width = 10;
+            Bounds.Height = 10;
+            TopSpeed = 500;
+
+            Position.X = pos.X;
+            Position.Y = pos.Y;
+
+            Velocity = dir;
+            if (Velocity.LengthSquared() > 0)
+                Velocity = Vector2.Multiply(Velocity, TopSpeed);
+
+            IsShotByPlayer = shotByPlayer;
+        }
+    }
+}
