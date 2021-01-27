6a34af51fe98e731a03824431c60569128a996a8
Matt Wrench <matt@mattwrench.com>
2021-01-26 18:45:36 -0600

    Add Input class

---
+++ Controllers/Input.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interstellar.Controllers
+{
+    static class Input
+    {
+        private static KeyboardState keyboardState, lastKeyboardState;
+        private static MouseState mouseState, lastMouseState;
+
+        public static void Update()
+        {
+            lastKeyboardState = keyboardState;
+            lastMouseState = mouseState;
+
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+        }
+
+        public static bool MoveLeft
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left);
+            }
+        }
+
+        public static bool MoveRight
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right);
+            }
+        }
+
+        public static bool MoveUp
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up);
+            }
+        }
+
+        public static bool MoveDown
+        {
+            get
+            {
+                return keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down);
+            }
+        }
+    }
+}
