93c485f6cd3eac6f39bd5f841ad212b9beb804ed
Matt Wrench <matt@mattwrench.com>
2021-01-31 08:58:02 -0600

    Track gamepad input

--- Controllers/Input.cs
+++ Controllers/Input.cs
@@ -8,10 +8,14 @@ namespace Interstellar.Controllers
 {
     static class Input
     {
+        public const float ThumbStickDeadZone = .1f;
+
         public static bool Attack = false;
+        public static bool IsUsingGamePad = false;
 
         private static KeyboardState keyboardState, lastKeyboardState;
         private static MouseState mouseState, lastMouseState;
+        private static GamePadState gamePadState, lastGamePadState;
         private static Camera2D camera;
 
         public static void Reset()
@@ -23,9 +27,14 @@ namespace Interstellar.Controllers
         {
             lastKeyboardState = keyboardState;
             lastMouseState = mouseState;
+            lastGamePadState = gamePadState;
 
             keyboardState = Keyboard.GetState();
             mouseState = Mouse.GetState();
+            gamePadState = GamePad.GetState(getPlayerIndex());
+
+            // Check for swap between gamepad and KBM
+            checkControlType();
 
             camera = cam;
 
@@ -33,6 +42,49 @@ namespace Interstellar.Controllers
                 Attack = true;
             else
                 Attack = false;
+        }
+
+        private static void checkControlType()
+        {
+            if (gamePadState.ThumbSticks.Left.LengthSquared() > ThumbStickDeadZone * ThumbStickDeadZone)
+                IsUsingGamePad = true;
+
+            if (keyboardState.GetPressedKeys().Length > 0 || 
+                mouseState.LeftButton == ButtonState.Pressed || 
+                mouseState.RightButton == ButtonState.Pressed)
+                IsUsingGamePad = false;
+        }
+
+        // Get lowest-indexed, connected controller
+        private static PlayerIndex getPlayerIndex()
+        {
+            if (GamePad.GetState(PlayerIndex.One).IsConnected)
+                return PlayerIndex.One;
+            else if (GamePad.GetState(PlayerIndex.Two).IsConnected)
+                return PlayerIndex.Two;
+            else if (GamePad.GetState(PlayerIndex.Three).IsConnected)
+                return PlayerIndex.Three;
+            else if (GamePad.GetState(PlayerIndex.Four).IsConnected)
+                return PlayerIndex.Four;
+            return PlayerIndex.One;
+        }
+
+        public static Vector2 LeftThumbstick
+        {
+            get
+            {
+                // Invert y-axis to match rest of game
+                return new Vector2(gamePadState.ThumbSticks.Left.X, -gamePadState.ThumbSticks.Left.Y);
+            }
+        }
+
+        public static Vector2 RightThumbstick
+        {
+            get
+            {
+                // Invert y-axis to match rest of game
+                return new Vector2(gamePadState.ThumbSticks.Right.X, -gamePadState.ThumbSticks.Right.Y);
+            }
         }
 
         public static Vector2 MouseScreenPos
